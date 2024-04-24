4ec49c49cb034c3b25a0c22437909b7743a59a78
RemxReg <regrethazraim@gmail.com>
2024-04-25 02:04:01 +0800

    Alpha version login responsiveness

--- login.php
+++ login.php
@@ -7,24 +7,33 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="font-sans antialiased text-gray-900 bg-gray-100">
-    <div class="w-full max-w-xs mx-auto mt-20">
-        <form action="login_process.php" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                    Username
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username">
+    <div class="w-full max-w-xs mx-auto mt-32 lg:flex lg:items-start lg:flex-row lg:gap-4">
+        <div class="lg:w-1/2">
+            <div class="lg:w-5/6">
+                <h1 class="text-3xl font-bold">LOGIN</h1>
+                <h2 class="text-sm mb-4">Use your LMS login credentials to access ECPS</h2>
             </div>
-            <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password">
-            </div>
-            <div class="flex items-center justify-between">
-                <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Sign In">
-            </div>
-        </form>
+        </div>
+        <div class="lg:w-full">
+            <form action="login_process.php" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 sm:bg-white sm:shadow-md sm:rounded px-8 pt-6 pb-8 mb-4 lg:ml-8 lg:mr-8">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-md font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username">
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-md font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password">
+                </div>
+                <div class="flex items-center justify-between">
+                    <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Sign In">
+                </div>
+            </form>
+        </div>
     </div>
+</body>
 </body>
 </html>
