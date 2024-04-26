63c3c014c43ee640047efd6401d94274a5e2291e
RemxReg <regrethazraim@gmail.com>
2024-04-26 17:02:40 +0800

    Alpha 2.0 Responsiveness

--- login.php
+++ login.php
@@ -8,28 +8,29 @@
 </head>
 <body class="font-sans antialiased text-gray-900 bg-gray-100">
     <div class="w-full max-w-xs mx-auto mt-32 lg:flex lg:items-start lg:flex-row lg:gap-4">
-        <div class="lg:w-1/2">
-            <div class="lg:w-5/6">
+        <div class="lg:w-[120%]">
+            <div class="lg:w-full">
                 <h1 class="text-3xl font-bold">LOGIN</h1>
                 <h2 class="text-sm mb-4">Use your LMS login credentials to access ECPS</h2>
             </div>
         </div>
-        <div class="lg:w-full">
-            <form action="login_process.php" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 sm:bg-white sm:shadow-md sm:rounded px-8 pt-6 pb-8 mb-4 lg:ml-8 lg:mr-8">
+        <div class="lg:w-">
+            <form action="login_process.php" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 sm:bg-white sm:shadow-md sm:rounded sm:px-4 sm:pt-3 sm:pb-4 sm:mb-2 lg:ml-8 lg:mr-8 lg:px-10 lg:pt-8 lg:pb-10 lg:mb-6 col-span-4">
                 <div class="mb-4">
                     <label class="block text-gray-700 text-md font-bold mb-2" for="username">
                         Username
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username">
+                    <input class="shadow appearance-none border rounded w-full py-3 px-8 lg:py-4 lg:px-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username">
                 </div>
                 <div class="mb-6">
                     <label class="block text-gray-700 text-md font-bold mb-2" for="password">
                         Password
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password">
+                    <input class="shadow appearance-none border rounded w-full py-3 px-8 lg:py-4 lg:px-20 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password">
                 </div>
                 <div class="flex items-center justify-between">
-                    <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Sign In">
+                    <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 lg:py-4 lg:px-8 rounded focus:outline-none focus:shadow-outline" type="submit" value="Sign In">
+                    <input class="bg-blue-500 hover:bg-blue-300 text-white font-bold py-3 px-8 lg:py-4 lg:px-8 rounded focus:outline-none focus:shadow-outline" type="submit" value="Sign Up">
                 </div>
             </form>
         </div>
