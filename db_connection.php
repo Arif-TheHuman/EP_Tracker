23e5d41aa539a5c52c1c8529111a0356ac39586a
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-05-05 20:30:10 +0800

    Refactor db_connection.php to create database if it doesn't exist and select it

--- db_connection.php
+++ db_connection.php
@@ -3,12 +3,17 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbName = "ECPS";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
-
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+// Create database if it doesn't exist
+$sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+if ($conn->query($sql) !== TRUE) {
+    die("Error creating database: " . $conn->error);
+}
+// Select the database
+$conn->select_db($dbName);
 ?>
