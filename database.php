5a96f2d01f3200e44253f211c45aa0956d9a60ed
RemxReg <regrethazraim@gmail.com>
2024-05-06 23:24:11 +0800

    Indoor clubs addition into database

--- database.php
+++ database.php
@@ -207,10 +207,12 @@ $sql = "CREATE TABLE IF NOT EXISTS clubs (
   }
 // Insert dummy data into clubs table
 $clubs = [
-    ['Volleyball Club', 'A club for volleyball enthusiasts.', 'outdoor', 20, 50, 'img1.jpg', '../assets/volleyballclubbg.png', '../assets/volleyballclub.png','../assets/volleyballclub.png', '../assets/volleyballclubbg.png','taskbarimg1.jpg'],
-    ['Touch Rugby Club', 'A club for rough and tough Rugby enthusiasts.', 'outdoor', 20, 50, 'img1.jpg', '../assets/rugbybg.png', '../assets/rugbyclub.png','../assets/rugbyclub.png', '../assets/rugbybg.png','taskbarimg1.jpg'],
-    ['Swimming Club', 'A club for fighting dreamers, fighting swimmers.', 'outdoor', 20, 50, 'img1.jpg', '../assets/swimmingclubbg.png', '../assets/swimmingclub.png', '../assets/swimmingclub.png', '../assets/swimmingclubbg.png','taskbarimg1.jpg'],
-    ['Chess Club', 'A club for those who enjoy playing chess.', 'indoor', 15, 30, 'img4.jpg', 'img5.jpg', 'img6.jpg','prof2.jpg', 'coverimg2.jpg','taskbarimg2.jpg'],
+    ['Volleyball Club', 'A club for volleyball enthusiasts.', 'outdoor', 20, 50, 'img1.jpg', '../assets/clubs/outdoor/volleyballclubbg.png', '../assets/clubs/outdoor/volleyballclub.png','../assets/clubs/outdoor/volleyballclub.png', '../assets/clubs/outdoor/volleyballclubbg.png','taskbarimg1.jpg'],
+    ['Touch Rugby Club', 'A club for rough and tough Rugby enthusiasts.', 'outdoor', 20, 50, 'img1.jpg', '../assets/clubs/outdoor/rugbybg.png', '../assets/clubs/outdoor/rugbyclub.png','../assets/clubs/outdoor/rugbyclub.png', '../assets/clubs/outdoor/rugbybg.png','taskbarimg1.jpg'],
+    ['Swimming Club', 'A club for fighting dreamers, fighting swimmers.', 'outdoor', 20, 50, 'img1.jpg', '../assets/clubs/outdoor/swimmingclubbg.png', '../assets/clubs/outdoor/swimmingclub.png', '../assets/clubs/outdoor/swimmingclub.png', '../assets/clubs/outdoor/swimmingclubbg.png','taskbarimg1.jpg'],
+    ['Kelab Nasyidul Islam', 'A club for those who enjoy playing chess.', 'indoor', 15, 30, 'img1.jpg', '../assets/clubs/indoor/knibg.png', '../assets/clubs/indoor/kni.png','../assets/clubs/indoor/kni.png', '../assets/clubs/indoor/knibg.png','taskbarimg2.jpg'],
+    ['Kelab Nasyidul Islam', 'A club for those who enjoy playing chess.', 'indoor', 15, 30, 'img1.jpg', '../assets/clubs/indoor/letsjapanbg.png', '../assets/clubs/indoor/letsjapan.png','../assets/clubs/indoor/letsjapan.png', '../assets/clubs/indoor/letsjapanbg.png','taskbarimg2.jpg'],
+    ['Kelab Nasyidul Islam', 'A club for those who enjoy playing chess.', 'indoor', 15, 30, 'img1.jpg', '../assets/clubs/indoor/artclubbg.png', '../assets/clubs/indoor/artclub.png','../assets/clubs/indoor/artclub.png', '../assets/clubs/indoor/artclubbg.png','taskbarimg2.jpg'],
     // Add more clubs as needed
 ];
 
